390fab1d76311d74f4e680fb5806a55d80c1be02
Sem Voigtlander <root@localhost.localdomain>
2017-11-09 21:56:37 +0100

    Added initial set of files and structure. Cleaned repository.

---
+++ MTerminal/bugs/respring/respring.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "respring.h"
+
+int respring() {
+	
+	//Open the respring payload from a webservice
+	
+	//Open the respring payload from a local file
+
+	//The payload is not long enough to cause an overflow
+
+	//A valid payload is received, start the overflow
+
+	//Check if the application is still running
+	return 0;
+}
---
+++ MTjFS/download-mtjfs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Downloading Mobile Terminal Jailed File System..."
+curl -s https://jailed.ml/access/MTjFS.fs.zip > MTjFS.fs.zip
+echo "[Done]"
+
